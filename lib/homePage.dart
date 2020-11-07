a04e43bef1a45c6d5b356834b80dcc32fa01ad35
Luis <39094995+lgon070@users.noreply.github.com>
2020-11-07 08:50:22 -0800

    Bottom navigation bar menu

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -1,19 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
-import 'package:material_floating_search_bar/material_floating_search_bar.dart';
-
-
 
 class HomePage extends StatefulWidget {
   @override
-  State<StatefulWidget> createState()  {
+  State<StatefulWidget> createState() {
     return _HomePageState();
   }
 }
 
 class _HomePageState extends State<HomePage> {
-
   LatLng _initialPosition = LatLng(34.0522, -118.2437);
   GoogleMapController _controller;
   Location _location = Location();
@@ -28,19 +24,79 @@ class _HomePageState extends State<HomePage> {
     });
   }
 
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        body: Stack(
-          children: <Widget>[
-            GoogleMap(
-              initialCameraPosition:
-              CameraPosition(target: _initialPosition, zoom: 10),
-              onMapCreated: _onMapCreated,
-              myLocationEnabled: true,
-            ),
-          ],
-        ),
-      );
-    }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Stack(
+        children: <Widget>[
+          GoogleMap(
+            initialCameraPosition:
+            CameraPosition(target: _initialPosition, zoom: 10),
+            onMapCreated: _onMapCreated,
+            myLocationEnabled: true,
+          ),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 0,
+        /*
+          Navigation Colors by Metro Rail Stations:
+            Gold: 0xffae08, (255,174,8)
+            Blue: 0x2371e7, (35,113,231)
+            Green: 0x0e884e, (14,136,78)
+            Red: 0xff2f00, (255,47,0)
+            Orange: 0xff7801, (255,120,1)
+            Purple: 0x780e67, (120,14,103)
+            Silver: 0xc3c3c3, (195,195,195)
+          */
+        items: [
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.home,
+                color: Color.fromRGBO(255,174,8, 1.0),
+                semanticLabel: "Home",
+              ),
+              label: "Home",
+              backgroundColor: Color.fromRGBO(195,195,195,0.5)
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.my_location,
+                color: Color.fromRGBO(35,113,231,1.0),
+                semanticLabel: "Current Location",
+              ),
+              label: "Location",
+              backgroundColor: Color.fromRGBO(195,195,195,0.5)
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.directions_bike,
+                color: Color.fromRGBO(14,136,78,1.0),
+                semanticLabel: "Metro Bike Stations",
+              ),
+              label: "Stations",
+              backgroundColor: Color.fromRGBO(195,195,195,0.5)
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.timeline,
+                color: Color.fromRGBO(255,47,0,1.0),
+                semanticLabel: "Find Directions to Bike Station",
+              ),
+              label: "Paths",
+              backgroundColor: Color.fromRGBO(195,195,195,0.5)
+          ),
+          BottomNavigationBarItem(
+              icon: Icon(
+                Icons.directions,
+                color: Color.fromRGBO(255,120,1,1.0),
+                semanticLabel: "Direction Steps to Bike Station",
+              ),
+              label: "Directions",
+              backgroundColor: Color.fromRGBO(195,195,195,0.5)
+          )
+        ],
+      ),
+    );
   }
+}
