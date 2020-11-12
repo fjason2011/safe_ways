915d8cb5a298a42d5391fb0bd2e560ac824dd5f5
Luis <39094995+lgon070@users.noreply.github.com>
2020-11-12 11:09:18 -0800

    main.dart is now the actual main. New android launch background to match the new splash screen. All metro bike share bike locations are now markers in the CurrentLocation.dart

--- lib/CurrentLocation.dart
+++ lib/CurrentLocation.dart
@@ -6,17 +6,25 @@ import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 
 class CurrentLocation extends StatefulWidget {
+  List _stationData;
+
+  //CurrentLocation constructor
+  CurrentLocation(this._stationData);
+
+  //Create state for _CurrentLocation, calls _CurrentLocation constructor
   @override
-  State<StatefulWidget> createState()  {
-    return _CurrentLocation();
-  }
+  _CurrentLocation createState() => _CurrentLocation(this._stationData);
 }
 
 class _CurrentLocation extends State<CurrentLocation> {
-
+  List _stationData = [];
+  List<Marker> _markers = [];
   LatLng _initialPosition = LatLng(34.0522, -118.2437);
   GoogleMapController _controller;
   Location _location = Location();
+
+  //_CurrentLocation constructor
+  _CurrentLocation(this._stationData);
 
   void _onMapCreated(GoogleMapController _ctrl) {
     _controller = _ctrl;
@@ -29,6 +37,23 @@ class _CurrentLocation extends State<CurrentLocation> {
   }
 
   @override
+  void initState(){
+    super.initState();
+    for(int i = 0; i < this._stationData.length; i++){
+      _markers.add(Marker(
+        markerId: MarkerId(this._stationData[i]['properties']['addressStreet']),
+        draggable: false,
+        position: LatLng(this._stationData[i]['properties']['latitude'], this._stationData[i]['properties']['longitude'])
+      ));
+    }
+    _markers.add(Marker(
+        markerId: MarkerId('testMarker'),
+        draggable: false,
+        position: LatLng(34.0485, -118.25854)
+    ));
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Stack(
@@ -38,6 +63,7 @@ class _CurrentLocation extends State<CurrentLocation> {
             CameraPosition(target: _initialPosition, zoom: 10),
             onMapCreated: _onMapCreated,
             myLocationEnabled: true,
+            markers: Set.from(_markers),
           ),
         ],
       ),
---
+++ lib/HomePage.dart
@@ -0,0 +1,153 @@
+import 'package:flutter/material.dart';
+import 'package:avatar_glow/avatar_glow.dart';
+import 'package:safe_ways/NavigationPage.dart';
+import 'package:safe_ways/delayed_animation.dart';
+
+//https://github.com/sagarshende23/Reflectly-Login-Screen
+//HomePage made using sagarshende23 Reflectly Login Screen splash page
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with SingleTickerProviderStateMixin {
+  final int delayedAmount = 500;
+  double _scale;
+  AnimationController _controller;
+  @override
+  void initState() {
+    _controller = AnimationController(
+      vsync: this,
+      duration: Duration(
+        milliseconds: 200,
+      ),
+      lowerBound: 0.0,
+      upperBound: 0.1,
+    )..addListener(() {
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final color = Colors.white;
+    _scale = 1 - _controller.value;
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+          backgroundColor: Color(0xFF8185E2),
+          body: Center(
+            child: Column(
+              children: <Widget>[
+                SizedBox(
+                  height: 50.0,
+                ),
+                AvatarGlow(
+                  endRadius: 90,
+                  duration: Duration(seconds: 2),
+                  glowColor: Colors.white24,
+                  repeat: true,
+                  repeatPauseDuration: Duration(seconds: 2),
+                  startDelay: Duration(seconds: 1),
+                  child: Material(
+                      elevation: 8.0,
+                      shape: CircleBorder(),
+                      child: CircleAvatar(
+                        backgroundColor: Colors.grey[100],
+                        child: FlutterLogo(
+                          size: 50.0,
+                        ),
+                        radius: 50.0,
+                      )),
+                ),
+                DelayedAnimation(
+                  child: Text(
+                    "Hi There!",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35.0,
+                        color: color),
+                  ),
+                  delay: delayedAmount + 1000,
+                ),
+                DelayedAnimation(
+                  child: Text(
+                    "I'm SafeWays",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 35.0,
+                        color: color),
+                  ),
+                  delay: delayedAmount + 2000,
+                ),
+                SizedBox(
+                  height: 30.0,
+                ),
+                DelayedAnimation(
+                  child: Text(
+                    "Your New Personal",
+                    style: TextStyle(fontSize: 20.0, color: color),
+                  ),
+                  delay: delayedAmount + 3000,
+                ),
+                DelayedAnimation(
+                  child: Text(
+                    "Traveling companion",
+                    style: TextStyle(fontSize: 20.0, color: color),
+                  ),
+                  delay: delayedAmount + 3000,
+                ),
+                SizedBox(
+                  height: 100.0,
+                ),
+                DelayedAnimation(
+                  child: GestureDetector(
+                    onTap: (){
+                      Navigator.push(context, MaterialPageRoute(builder: (context) => NavigationPage()));
+                    },
+                    onTapDown: _onTapDown,
+                    onTapUp: _onTapUp,
+                    child: Transform.scale(
+                      scale: _scale,
+                      child: _animatedButtonUI,
+                    ),
+                  ),
+                  delay: delayedAmount + 4000,
+                ),
+
+
+              ],
+            ),
+          )
+      ),
+    );
+  }
+
+  Widget get _animatedButtonUI => Container(
+    height: 60,
+    width: 270,
+    decoration: BoxDecoration(
+      borderRadius: BorderRadius.circular(100.0),
+      color: Colors.white,
+    ),
+    child: Center(
+      child: Text(
+        'Let\'s get started',
+        style: TextStyle(
+          fontSize: 20.0,
+          fontWeight: FontWeight.bold,
+          color: Color(0xFF8185E2),
+        ),
+      ),
+    ),
+  );
+
+  void _onTapDown(TapDownDetails details) {
+    _controller.forward();
+  }
+
+  void _onTapUp(TapUpDetails details) {
+    _controller.reverse();
+  }
+}
